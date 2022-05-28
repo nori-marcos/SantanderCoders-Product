39297ce644a7bb4cc8b466ebca5c5d4f0b7e71ee
samuelnfavero <faveronsamuel@gmail.com>
2022-05-28 17:01:46 -0300

    feat:Implementacao do Swagger

--- src/main/java/com/letscode/Product/DTO/ProductRequest.java
+++ src/main/java/com/letscode/Product/DTO/ProductRequest.java
@@ -1,18 +1,28 @@
 package com.letscode.Product.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 public class ProductRequest {
 
-    private String nome;
+    
+    @Schema(description = "Nome do produto", example = "Geladeira")
+    private String name;
 
+    @NotNull
+    @Schema(description = "Preço do produto", example = "2000")
     private BigDecimal price;
 
+    @NotNull
+    @Min(1)
+    @Schema(description = "Quantidade de produtos para salvar no estoque", example = "1")
     private  int quantity;
 
 }
