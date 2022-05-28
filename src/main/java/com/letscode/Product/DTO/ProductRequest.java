43335a02a2de54c0acb790e8029a5ea5ae5a853f
nori-marcos <marcos.noriyuki.miyata@gmail.com>
2022-05-28 12:19:55 -0300

    feat: Criacao dos dtos

--- src/main/java/com/letscode/Product/DTO/ProductRequest.java
+++ src/main/java/com/letscode/Product/DTO/ProductRequest.java
@@ -1,2 +1,17 @@
-package com.letscode.Product.DTO;public class ProductRequest {
+package com.letscode.Product.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductRequest {
+
+    private String nome;
+
+    private String cpf;
+
+    private String senha;
+
+    private String email;
 }
--- src/main/java/com/letscode/Product/DTO/ProductResponse.java
+++ src/main/java/com/letscode/Product/DTO/ProductResponse.java
@@ -1,4 +1,24 @@
-package com.letscode.Product.controller;
+package com.letscode.Product.DTO;
 
+import com.letscode.Product.model.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductResponse {
+
+    private String uuid;
+    private String nome;
+    private String cpf;
+    private String senha;
+    private String email;
+    public ProductResponse(Product product) {
+        this.uuid = product.getUuid();
+        this.nome = product.getNome();
+        this.cpf = product.getCpf();
+        this.email = product.getEmail();
+        this.senha = product.getSenha();
+    }
+
 }
