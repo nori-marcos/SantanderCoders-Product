b553ebf2e04d0a04fc210d94f9eb18e07a3506b8
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 13:06:41 -0300

    feat: habilitar eureka client

--- src/main/java/com/letscode/Product/ProductApplication.java
+++ src/main/java/com/letscode/Product/ProductApplication.java
@@ -3,9 +3,11 @@ package com.letscode.Product;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableEurekaClient
 public class ProductApplication {
 
     public static void main(String[] args) {
